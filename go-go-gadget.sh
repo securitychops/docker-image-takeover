bbe1076b9e5cf8020cd41783fac7e58e3785c61d
Security Chops <39027035+securitychops@users.noreply.github.com>
2020-02-16 11:24:47 -0500

    Update go-go-gadget.sh

--- go-go-gadget.sh
+++ go-go-gadget.sh
@@ -19,3 +19,6 @@ done
 takeover -T 3 -t 20 -l /tmp/full-domains.txt -o takeover.json
 
 python3 process.py
+
+echo '{"task_type":"slack","body":"takeover complete : '$SCAN_ME'"}' > /tmp/$UUID
+aws sqs send-message --queue-url $SQS_URL --message-body $(cat /tmp/$UUID)
