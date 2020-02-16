5e2d4baa9812d9c33bc0a729407f5475beb047ae
Security Chops <39027035+securitychops@users.noreply.github.com>
2020-02-16 10:36:59 -0500

    Create go-go-gadget.sh

---
+++ go-go-gadget.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir -p ~/.aws/
+echo "[default]" >> ~/.aws/credentials
+echo "aws_access_key_id = $S3_BUCKET_KEY" >> ~/.aws/credentials
+echo "aws_secret_access_key = $S3_BUCKET_SECRET" >> ~/.aws/credentials
+
+echo "[default]" >> ~/.aws/config
+echo "region = $AWS_REGION" >> ~/.aws/config
+
+aws s3 mv /tmp/domains.txt s3://$S3_BUCKET_NAME/tmp/$UUID
+
+for word in $(cat /tmp/domains.txt);
+do
+  echo "http://$word" >> /tmp/full-domains.txt
+  echo "https://$word" >> /tmp/full-domains.txt
+done
+
+takeover -T 3 -t 20 -l /tmp/full-domains.txt -o takeover.json
+
+python3 process.py
